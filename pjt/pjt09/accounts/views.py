3c7a845b7d731c9f13e15a28a9a2acb062980295
eunyoung <eunyoung950210@gmail.com>
2022-05-08 20:53:15 +0900

    login completed

--- pjt/pjt09/accounts/views.py
+++ pjt/pjt09/accounts/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 
 
 @require_http_methods(["GET", "POST"])
 def signup(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+
     if request.method == 'POST':
         signupform = CustomUserCreationForm(request.POST)
         if signupform.is_valid():
             signupform.save()
             return redirect('movies:index')
-    if request.method == 'GET':
+    else:
         signupform = CustomUserCreationForm()
     context = {
         'signupform': signupform,
@@ -19,8 +23,20 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 
+@require_http_methods(["GET", "POST"])
 def login(request):
+    if request.user.is_authenticated:
+        return redirect('movies:index')
+        
     if request.method == 'POST':
-        pass
+        loginform = AuthenticationForm(request, data=request.POST)
+        if loginform.is_valid():
+            auth_login(request, loginform.get_user())
+            print('여기여기여기')
+            return redirect('movies:index')
     else:
         loginform = AuthenticationForm()
+    context = {
+        'loginform': loginform,
+    }
+    return render(request, 'accounts/login.html', context)
