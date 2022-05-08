4f76afe31e0a6fd3945ae60f7b43b3892b80a403
eunyoung <eunyoung950210@gmail.com>
2022-05-08 20:25:31 +0900

    authenticationform

--- pjt/pjt09/accounts/views.py
+++ pjt/pjt09/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.views.decorators.http import require_http_methods
+from django.contrib.auth.forms import AuthenticationForm
 
 
 @require_http_methods(["GET", "POST"])
@@ -16,3 +17,10 @@ def signup(request):
         'signupform': signupform,
     }
     return render(request, 'accounts/signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        loginform = AuthenticationForm()
